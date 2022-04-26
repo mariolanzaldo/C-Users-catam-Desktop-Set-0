5cd1d2c9f6a2f75c3fb5de7a76d1bf877995632e
mariolanzaldo <100377731+mariolanzaldo@users.noreply.github.com>
2022-04-26 18:54:50 -0500

    Update E3.1.js

--- E3.1.js
+++ E3.1.js
@@ -1,22 +1,9 @@
-// let a = 1;
-// let b = 23;
-let z = [];
-let array = ["A","B","C","D","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
- 
-
-let B = function (a,b) {
+function base13(a, b) {
     let x;
     x = a * b;
-    for (i=x; 0 < i; i = Math.floor(i/13)){
-        if(i % 13 >= 10){
-            z.push(array[i % 13 - 10]);
-        } 
-        else{
-            z.push(i % 13);
-        }
-    }
-    z = z.reverse();
-    z = z.join("");
-    console.log(z);
-};
-B(1,27);
+    let result = parseInt(String(x)).toString(13);
+    return result;
+}
+
+let A = base13(1, 27);
+console.log(A);
