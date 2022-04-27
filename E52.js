f69fa4c2ed1fcf600ac3d079fe9d51de695d1282
mariolanzaldo <100377731+mariolanzaldo@users.noreply.github.com>
2022-04-27 16:14:48 -0500

    Update E52.js

--- E52.js
+++ E52.js
@@ -1,36 +1,38 @@
-const building = { person:{ Mario: {floor: "1", room: "3", equipment: "Asus"},
-                            Martha: {floor: "2", room: "2", equipment: "dell"},
-                            Louis: {floor: "1", room: "1", equipment: "HP"},
-                            Mark: {floor: "2", room: "1", equipment: "Acer"}
-                },
-                equipment:{ dell:{floor: "2", room: "2",user: "Martha"},
-                           HP: {floor: "1", room: "1",user: "Louis"},
-                            Asus: {floor: "1", room: "3",user: "Martha"},
-                            Acer: {floor: "2", room: "1",user: "Mark"}
-                }
+const building = {
+    person: {
+        Mario: { floor: "1", room: "3", equipment: "Asus" },
+        Martha: { floor: "2", room: "2", equipment: "dell" },
+        Louis: { floor: "1", room: "1", equipment: "HP" },
+        Mark: { floor: "2", room: "1", equipment: "Acer" }
+    },
+    equipment: {
+        dell: { floor: "2", room: "2", user: "Martha" },
+        HP: { floor: "1", room: "1", user: "Louis" },
+        Asus: { floor: "1", room: "3", user: "Martha" },
+        Acer: { floor: "2", room: "1", user: "Mark" }
+    }
 };
 
 
-function memoizeFind(){
+function memoizeFind() {
     let cache = {};
-    return function Find(object){
-        if(object in cache){
-            console.log(`In memory`);
+    return function find(object) {
+        if (object in cache) {
             return cache[object];
         }
         else {
-           if(Object.keys(building["person"]).indexOf(object) >= 0){
-            cache[object] = building["person"][object];
-           } else if(Object.keys(building["equipment"]).indexOf(object) >= 0) {
-            cache[object] = building["equipment"][object];
-           }
+            if (Object.keys(building["person"]).indexOf(object) >= 0) {
+                cache[object] = building["person"][object];
+            } else if (Object.keys(building["equipment"]).indexOf(object) >= 0) {
+                cache[object] = building["equipment"][object];
+            }
         }
-return cache[object];
-}
+        return cache[object];
+    }
 
 }
 
-let memoize =  memoizeFind();
+let memoize = memoizeFind();
 console.log(memoize("Mario"));
 console.log(memoize("HP"));
 console.log(memoize("Mario"));
