eaf4ea1e469320be18b9de7fc1bd7a16b41c8c5a
mariolanzaldo <100377731+mariolanzaldo@users.noreply.github.com>
2022-04-29 11:24:27 -0500

    Update and rename E132.js to E13.2.js

--- E13.2.js
+++ E13.2.js
@@ -5,25 +5,25 @@ btn.style.fontSize = '20px';
 met.style.width = '200px';
 met.style.height = '20px';
 
-let increment = (function() {
+let increment = (function () {
     let i = 1;
     return function () {
         return i++
     }
 })();
 
-btn.addEventListener('click',() => {
-let counter = increment ();
-    if(counter === 1){
-      met.value = '33';
+btn.addEventListener('click', () => {
+    let counter = increment();
+    if (counter === 1) {
+        met.value = counter;
     }
-    else if (counter ===2){
-        met.value = '67';
+    else if (counter === 2) {
+        met.value = counter;
     }
-    else if (counter === 3){
-        met.value = '100';
+    else if (counter === 3) {
+        met.value = counter;
         alert('Button is disabled');
-        btn.disabled = true; 
+        btn.disabled = true;
         // This alternative removes the button
         // btn.remove();
         // met.style.transform ='translate(100%,0)';
