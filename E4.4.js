23b51959f7c05a01e1fccea16b665212ec1ad928
mariolanzaldo <100377731+mariolanzaldo@users.noreply.github.com>
2022-05-06 17:27:28 -0500

    Update E4.4.js

--- E4.4.js
+++ E4.4.js
@@ -1,22 +1,12 @@
 function dataType(...args) {
     let type = [];
-    for (let i = 0; i < args.length; i++) {
-        if (typeof (args[i]) === 'number') {
-            if (Math.floor(args[i]) != args[i]) {
-                type[i] = "Float";
-            } else {
-                type[i] = "Number"
-            }
-        } else if (typeof args[i] === 'object') {
-            if (Array.isArray(args[i])) {
-                type[i] = "Array";
-            } else {
-                type[i] = "Object";
-            }
-        } else if (typeof args[i] === 'string') {
-            type[i] = "String";
-        } else if (typeof args[i] === 'function') {
-            type[i] = "Function";
+    for (let i in args) {
+        if (!isNaN(Math.floor(args[i])) && Math.floor(args[i]) != args[i]) {
+            type[i] = `float`;
+        } else if (Array.isArray(args[i])) {
+            type[i] = `array`;
+        } else {
+            type[i] = `${typeof args[i]}`;
         }
     }
     return type;
@@ -25,4 +15,4 @@ function dataType(...args) {
 console.log(dataType('a', 1.3, {
     a: 8,
     b: 'P'
-}, function () { }, 2, ['problem', 'cat']));
+}, function () { }, 2, ['problem', 'cat'], true));
