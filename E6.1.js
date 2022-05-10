740aaa0a81a4a38076e315e53df3376a682e3b24
mariolanzaldo <100377731+mariolanzaldo@users.noreply.github.com>
2022-05-10 11:44:51 -0500

    Update E6.1.js

--- E6.1.js
+++ E6.1.js
@@ -17,7 +17,6 @@ class Shape extends ObjectCreator {
         }
 
         else if (this.edges === 4) {
-            // return `Quadrilateral is shape since it has ${this.edges} edges`;
             if (this.width === this.height) {
                 return `Quadrilateral is shape(${this.edges} edges). However, this a square since ${this.width} and ${this.height} are equal`;
             } else {
@@ -27,7 +26,7 @@ class Shape extends ObjectCreator {
     }
 }
 
-class Quadrilateral extends Shape {
+class Square extends Shape {
     constructor(...args) {
         super(...args);
         this.name = 'square';
@@ -53,7 +52,7 @@ class Rectangle extends Shape {
         return `The ${this.name} area is: ${area}`;
     }
     perimeter() {
-        let perimeter = (this.width * 2) + (this.height * 2);
+        let perimeter = 2 * (this.width + this.height);
         return `The ${this.name} perimeter is: ${perimeter}`;
     }
 }
@@ -96,7 +95,7 @@ class Triangle extends Shape {
     }
 }
 
-let square = new Quadrilateral(5, 5, 4);
+let square = new Square(5, 5, 4);
 let rectangle = new Rectangle(4, 3, 4);
 let triangle = new Triangle(3, 5, 3, 4, 4);
 
@@ -112,3 +111,91 @@ console.log(triangle.display());
 console.log(triangle.area());
 console.log(triangle.perimeter());
 console.log(triangle.triangleType())
+
+//Another example
+class Animal {
+    constructor(name) {
+        this.name = name;
+    }
+
+    greet() {
+        console.log(`Hi, my name is ${this.name}`)
+    };
+
+    behaviour() {
+
+    };
+}
+
+class Vertebrate extends Animal {
+    constructor(...args) {
+        super(...args);
+        this.spine = 'spine';
+    }
+}
+
+class Invertebrate extends Animal {
+    constructor(...args) {
+        super(...args);
+        this.spine = 'no-spine';
+    }
+}
+
+class Octopus extends Invertebrate {
+    constructor(...args) {
+        super(...args);
+        this.behave = 'solitary'
+        this.sound = 'no-sound'
+    }
+    behaviour() {
+        console.log(`I'm an octopus and I have ${this.spine}. Octopus is a ${this.behave} animal. I'm not able to emit any ${this.sound}. However, I can camuflage!`);
+    }
+}
+
+class Shark extends Vertebrate {
+    constructor(...args) {
+        super(...args);
+        this.behave = 'dangerous'
+        this.sound = 'sound'
+    }
+    behaviour() {
+        console.log(`I'm a shark. Sharks have ${this.spine}. Sharks are considered ${this.behave}. Fun fact: my organs are not able to produce any ${this.sound}`);
+    }
+}
+
+class Bird extends Vertebrate {
+    constructor(...args) {
+        super(...args);
+        this.behave = 'instintive'
+        this.sound = 'whistle'
+    }
+
+    bird() {
+        console.log(`I'm a bird. Then, I have ${this.spine}`);
+    }
+}
+
+class BlueJay extends Bird {
+    constructor(...args) {
+        super(...args);
+        this.behave = 'aggressive'
+        this.spSound = 'queedle, queedle, queedle'
+    }
+    behaviour() {
+        console.log(`Also, I'm a blue jay. We're considered ${this.behave} birds. Besides, I ${this.sound}, but people knows me to sing: ${this.spSound}!`);
+    }
+}
+
+const paul = new Octopus("Paul");
+const peter = new Shark("Peter");
+const lisa = new BlueJay("Lisa");
+
+paul.greet();
+paul.behaviour();
+
+peter.greet();
+peter.behaviour();
+
+lisa.greet();
+lisa.bird();
+lisa.behaviour();
