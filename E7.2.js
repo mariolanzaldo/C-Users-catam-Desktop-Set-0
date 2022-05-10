77c84cd850ded61d7419d01aaad412dd09b73299
mariolanzaldo <100377731+mariolanzaldo@users.noreply.github.com>
2022-05-10 13:03:16 -0500

    Update E7.2.js

--- E7.2.js
+++ E7.2.js
@@ -1,33 +1,37 @@
 function transform(date) {
     const validInput = /\d{1,2}-\d{1,2}-\d{4}$/i.exec(date);
     const holidays = {
-        "Jour de l'an": "01-01",
-        "Fête du premier mai": "01-03",
-        "Jour de la Victoire": "08-03",
-        "Jour de l'Ascension": "26-05",
-        "Fête nationale": "14-08",
-        'Assomption': "15-09",
-        "La Toussaint": "01-11",
-        "Jour d'armistice": "11-11",
-        "Noël": "25-12"
+        "01-01": "Jour de l'an",
+        "01-03": "Fête du premier mai",
+        "08-03": "Jour de la Victoire",
+        "26-05": "Jour de l'Ascension",
+        "14-08": "Fête nationale",
+        '15-09': "Assomption",
+        "01-11": "La Toussaint",
+        "11-11": "Jour d'armistice",
+        "25-12": "Noël"
     };
     const keys = Object.keys(holidays);
 
     if (!validInput) {
         throw new Error('Invalid Input')
-    }
-
-    let arr = date.split("-");
-    let x = [arr[1], arr[0], arr[2]];
-    let newDate = x.join('-');
-    x = [arr[1], arr[0]].join('-');
-    let matchDay = keys.find(function (key) {
-        return holidays[key] === x;
-    });
-    if (matchDay !== undefined) {
-        return `French-FRAN: ${newDate}` + ' ' + `${matchDay}`;
     } else {
-        return `French-FRAN: ${newDate}`;
+        let arr = date.split("-");
+        let x = [arr[1], arr[0], arr[2]];
+        let newDate = x.join('-');
+        x = [arr[1], arr[0]].join('-');
+
+        let matchDay;
+        for (let property in holidays) {
+            if (property === x) {
+                matchDay = holidays[property];
+            }
+        }
+        if (matchDay !== undefined) {
+            return `French-FRAN: ${newDate}` + ' ' + `${matchDay}`;
+        } else {
+            return `French-FRAN: ${newDate}`;
+        }
     }
 }
 console.log(transform("05-26-2022"));
