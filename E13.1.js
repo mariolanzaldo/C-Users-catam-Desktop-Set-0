1d9d7f373b53c576f3d037c08cc8a7a3a1e13c6d
mariolanzaldo <100377731+mariolanzaldo@users.noreply.github.com>
2022-05-11 14:11:07 -0500

    Update E13.1.js

--- E13.1.js
+++ E13.1.js
@@ -1,27 +1,27 @@
 function createTable() {
   let selectedId = document.getElementById('table');
+  let fragment = document.createDocumentFragment();
   for (let i = 1; i < 6; i++) {
     let tr = document.createElement('TR');
-    selectedId.appendChild(tr);
+    fragment.appendChild(tr);
     for (let j = 1; j < 6; j++) {
-      console.log(i);
       let td = document.createElement('TD');
       td.classList.add("click");
       td.style.background = randColor();
-      i === 1 ? td.innerHTML = j : i === 2 ? td.innerHTML = j + 5 : i === 3 ? td.innerHTML = j + 10 : i === 4 ? td.innerHTML = j + 15 : td.innerHTML = j + 20;
+      i === 1 ? td.textContent = j : i === 2 ? td.textContent = j + 5 : i === 3 ? td.textContent = j + 10 : i === 4 ? td.innerHTML = j + 15 : td.textContent = j + 20;
       tr.appendChild(td);
     }
   }
-  // let color = cell.style.backgroundColor;
-  let cell = document.getElementsByClassName("click");
-  for (let i in cell) {
-    cell[i].onclick = function () {
-      if (cell[i].style.backgroundColor === 'grey') {
-        alert(cell[i].innerHTML);
-      } else {
-        cell[i].style.background = 'grey';
-        alert(cell[i].innerHTML);
-      }
+  let clone = fragment.cloneNode(true);
+  selectedId.appendChild(clone);
+
+  selectedId.onclick = function (event) {
+    let target = event.target;
+    if (target.tagName === 'TD' && target.style.backgroundColor === 'grey') {
+      alert(target.textContent);
+    } else {
+      target.style.backgroundColor = 'grey';
+      alert(target.textContent);
     }
   }
 }
