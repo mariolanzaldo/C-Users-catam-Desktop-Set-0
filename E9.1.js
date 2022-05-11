a4682966a10005e9a274ff3415f8027db4cebeae
mariolanzaldo <100377731+mariolanzaldo@users.noreply.github.com>
2022-05-11 13:27:20 -0500

    Update E9.1.js

--- E9.1.js
+++ E9.1.js
@@ -1,5 +1,5 @@
 function dataParse(str) {
-    let parsedObj = JSON.parse(str);
+    const parsedObj = JSON.parse(str);
     const body = /[^{\}]+(?=})/g;
     const args = /(?<value>(?<=\().*(?=\)))/g;
     const argFunc = parsedObj.myFn.match(args);
@@ -9,7 +9,7 @@ function dataParse(str) {
 }
 
 
-let str = '{"prop1": 42, "myFn":"function(a,b){return a+b+this.prop1;}"}';
+const str = '{"prop1": 42, "myFn":"function(a,b){return a+b+this.prop1;}"}';
 let newObj = dataParse(str);
 console.log(newObj);
 console.log(newObj.myFn(1, 2));
