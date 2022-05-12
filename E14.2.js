29ab859df74ef882e66324d2c1cd8fa174019136
mariolanzaldo <100377731+mariolanzaldo@users.noreply.github.com>
2022-05-12 12:03:35 -0500

    Update E14.2.js

--- E14.2.js
+++ E14.2.js
@@ -1,11 +1,17 @@
 function getInput() {
-    const container = document.querySelector('div');
+    const container = document.querySelector('.wrapper');
     let input = document.querySelector("input").value;
+    const fragment = document.createDocumentFragment();
+    const temp = document.querySelector('.temp').content;
+    const clone = document.importNode(temp, true);
+
+    fragment.appendChild(clone);
     input = parseInt(input);
     if (isNaN(input) === false) {
         if (input <= 4 && input > 1) {
-            container.style.removeProperty('--n-col');
-            container.style.setProperty('--n-col', input);
+            container.remove();
+            document.body.appendChild(fragment);
+            document.querySelector(".wrapper").style.setProperty('--n-col', input);
         } else {
             alert('Only columns between 2-4 are accepted');
         }
