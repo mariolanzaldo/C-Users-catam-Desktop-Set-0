d15044a80c7dcf1eac661726a1e0a7f726f3d2a8
mariolanzaldo <100377731+mariolanzaldo@users.noreply.github.com>
2022-05-19 16:34:19 -0500

    Update E4.3.js

--- E4.3.js
+++ E4.3.js
@@ -1,30 +1,19 @@
-class HoldObj {
-  constructor(a) {
-    if (isNaN(parseInt(a))) {
-      throw new Error('Must be a number');
-    } else {
-      this[a] = a;
-    }
-  }
-}
-const value = 2;
-const newObj = new HoldObj(value);
+clet newObj = (function () {
+  let objects = {};
 
-let newObjProxy = new Proxy(newObj, {
-  get: (obj, prop) => {
-    if (prop == `${value}`) {
-      return new Error(prop + '- is private');
+  return {
+    set: function (num) {
+      if (!isNaN(parseInt(num))) {
+        objects.num = num;
+      } else {
+        throw new Error(`Must be a number`);
+      }
+    },
+    get: function () {
+      return objects;
     }
-    return obj[prop];
-  },
-  set: (obj, prop, val) => {
-    if (prop == `${value}`) {
-      throw new Error(prop + '- is private');
-    }
-    obj[prop] = val;
-    return true;
-  }
-});
+  };
+})();
 
-newObjProxy[value] = 3;
-console.log(newObjProxy);
+newObj.set(4);
+console.log(newObj.get());
