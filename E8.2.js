e609c54842cbb265b9b2308237ad25dea1a33843
mariolanzaldo <100377731+mariolanzaldo@users.noreply.github.com>
2022-05-19 21:03:36 -0500

    Update E8.2.js

--- E8.2.js
+++ E8.2.js
@@ -11,10 +11,9 @@ function c() {
 }
 
 function myFunction(counter) {
-    let timer = 0;
     if (counter !== 0 && counter % 2 === 0) a();
     if (counter !== 0 && counter % 4 === 0) b();
     if (counter !== 0 && counter % 5 === 0) c();
-    setTimeout(myFunction, timer + 15000, counter + 1);
+    setTimeout(myFunction, 15000, counter + 1);
 }
 myFunction(0);
