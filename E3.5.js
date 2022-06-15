02dc8748685961da2dbcf79a2bc683f59dc712f8
mariolanzaldo <100377731+mariolanzaldo@users.noreply.github.com>
2022-06-15 13:58:31 -0500

    Update E3.5.js

--- E3.5.js
+++ E3.5.js
@@ -1,27 +1,27 @@
-function printObjProp(object, boolean) {
-  let output = [];
-  for (let key in object) {
-    if (boolean && object.hasOwnProperty(key)) {
-      output.push(key);
-    } else if (!boolean) {
-      output.push(key);
+function printObjProp(object, instance) {
+    let output = [];
+    for (let key in object) {
+      if (instance && object.hasOwnProperty(key)) {
+        output.push(key);
+      } else if (!instance) {
+        output.push(key);
+      }
     }
+    return output
   }
-  return output
-}
-
-function CustomObject(a, b) {
-  this.a = a;
-  this.b = b;
-};
-
-CustomObject.prototype.c = function () {
-  return this.a + this.b;
-}
-
-CustomObject.prototype.s = function () {
-  return this.a - this.b;
-}
-
-let newObject = new CustomObject(3, 2);
-console.log(printObjProp(newObject));
+  
+  function CustomObject(a, b) {
+    this.a = a;
+    this.b = b;
+  };
+  
+  CustomObject.prototype.c = function () {
+    return this.a + this.b;
+  }
+  
+  CustomObject.prototype.s = function () {
+    return this.a - this.b;
+  }
+  
+  let newObject = new CustomObject(3, 2);
+  console.log(printObjProp(newObject));
